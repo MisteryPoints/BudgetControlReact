bf3cd810c2ea68f7a3c711736c81862b60a7733c
Víctor Tejada <105092810+MisteryPoints@users.noreply.github.com>
2022-07-16 15:19:51 +0000

    first commit

---
+++ budget-control/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NuevoPresupuesto from './components/NuevoPresupuesto'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>Planificador de Gastos</h1>
+        <NuevoPresupuesto/>
+        <NuevoPresupuesto/>
+    </header>
+  )
+}
+
+export default Header
